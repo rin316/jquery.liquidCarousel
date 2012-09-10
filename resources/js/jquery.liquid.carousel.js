6f7e52482ec6fc8437641153007d271012fe1f8a
Yuta Hayashi (skillpartners) <hayashi@skillpartners.co.jp>
2012-09-11 05:27:50 +0900

    プラグイン呼び出しをmain.js に移動

--- resources/js/jquery.liquid.carousel.js
+++ resources/js/jquery.liquid.carousel.js
@@ -5,30 +5,6 @@
 ;(function ($, window, undefined) {
 
 
-//window load
-$(window).load(function(){
-	$('.mod-topContents').liquidCarousel({
-		listSelector: '.mod-topContents-visual-list'
-	  , itemSelector: '.mod-topContents-visual-item'
-	  , controlSelector: '.mod-topContents-controller'
-	  , controlListSelector: '.mod-topContents-controller-list'
-	  , controlItemSelector: '.mod-topContents-controller-item'
-	  , prevSelector: '.mod-topContents-prev'
-	  , nextSelector: '.mod-topContents-next'
-	  , x_position: 'left'
-	  , loop: true
-	  , speed: 300
-	  , currentClass: 'mod-topContents-current'
-	  , currentHighlight: true
-	  , currentNumber: 1
-	  //, autoPlay: true
-	  //, autoInterval: 1000
-	});
-});
-
-
-
-
 var Carousel
 	, DEFAULT_OPTIONS
 	;
--- resources/js/main.js
+++ resources/js/main.js
@@ -17,4 +17,26 @@ var addJsOnClass = function () {
 addJsOnClass();
 
 
+//window load
+$(window).load(function(){
+	$('.mod-topContents').liquidCarousel({
+		listSelector: '.mod-topContents-visual-list'
+	  , itemSelector: '.mod-topContents-visual-item'
+	  , controlSelector: '.mod-topContents-controller'
+	  , controlListSelector: '.mod-topContents-controller-list'
+	  , controlItemSelector: '.mod-topContents-controller-item'
+	  , prevSelector: '.mod-topContents-prev'
+	  , nextSelector: '.mod-topContents-next'
+	  , x_position: 'center'
+	  , loop: true
+	  , speed: 300
+	  , currentClass: 'mod-topContents-current'
+	  , currentHighlight: true
+	  , currentNumber: 1
+	  //, autoPlay: true
+	  //, autoInterval: 1000
+	});
+});
+
+
 })(jQuery, this);
