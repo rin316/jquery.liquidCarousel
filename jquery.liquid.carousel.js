dd9f05248501bcb629ed054bfb59ef25b14fd407
Yuta Hayashi (skillpartners) <hayashi@skillpartners.co.jp>
2012-09-26 16:41:27 +0900

    refact: var __this >> self

--- jquery.liquid.carousel.js
+++ jquery.liquid.carousel.js
@@ -45,32 +45,32 @@ DEFAULT_OPTIONS = {
  * Carousel
  */
 Carousel = function ($element, options) {
-	var __this = this;
+	var self = this;
 	
-	__this.o = $.extend({}, DEFAULT_OPTIONS, options);
+	self.o = $.extend({}, DEFAULT_OPTIONS, options);
 	
-	__this.$element =        $element;
-	__this.$list =           __this.$element.find($(__this.o.listSelector));
-	__this.$item =           __this.$element.find($(__this.o.itemSelector));
-	__this.$paginationList = __this.$element.find($(__this.o.paginationListSelector));
-	__this.$paginationItem = __this.$element.find($(__this.o.paginationItemSelector));
-	__this.$prevNavi =       __this.$element.find($(__this.o.prevSelector));
-	__this.$nextNavi =       __this.$element.find($(__this.o.nextSelector));
-	__this.$allList =        __this.$paginationList.add(__this.$list);
-	__this.$allItem  =       __this.$paginationItem.add(__this.$item);
-	__this.$allListAndNavi = __this.$allList.add(__this.$prevNavi).add(__this.$nextNavi);
+	self.$element =        $element;
+	self.$list =           self.$element.find($(self.o.listSelector));
+	self.$item =           self.$element.find($(self.o.itemSelector));
+	self.$paginationList = self.$element.find($(self.o.paginationListSelector));
+	self.$paginationItem = self.$element.find($(self.o.paginationItemSelector));
+	self.$prevNavi =       self.$element.find($(self.o.prevSelector));
+	self.$nextNavi =       self.$element.find($(self.o.nextSelector));
+	self.$allList =        self.$paginationList.add(self.$list);
+	self.$allItem  =       self.$paginationItem.add(self.$item);
+	self.$allListAndNavi = self.$allList.add(self.$prevNavi).add(self.$nextNavi);
 	
-	__this.elementWidth = __this.$element.outerWidth(true);
-	__this.itemWidth = __this.$item.outerWidth(true);
+	self.elementWidth = self.$element.outerWidth(true);
+	self.itemWidth = self.$item.outerWidth(true);
 	
-	__this.clonePrependNum = 0;
-	__this.cloneAppendNum = 0;
-	__this.index = __this.o.index - 1;
-	__this.isMoving = false;
+	self.clonePrependNum = 0;
+	self.cloneAppendNum = 0;
+	self.index = self.o.index - 1;
+	self.isMoving = false;
 	
-	__this.init();
+	self.init();
 	
-	return __this;
+	return self;
 };//Carousel
 
 
@@ -83,40 +83,40 @@ Carousel.prototype = {
 	 * init
 	 */
 	init: function () {
-		var __this = this;
+		var self = this;
 		
 		//indexを更新
-		__this.indexUpdate(__this.index);
+		self.indexUpdate(self.index);
 		
 		//左右にクローン作成
-		if (__this.o.loop) { __this.makeClone(); }
+		if (self.o.loop) { self.makeClone(); }
 		
 		//$listのmargin, widthを設定
-		__this.setListStyle();
+		self.setListStyle();
 		
 		//current表示
-		__this.addCurrentClass();
-		if (__this.o.currentHighlight) { __this.highlightEffect(); }
+		self.addCurrentClass();
+		if (self.o.currentHighlight) { self.highlightEffect(); }
 		
 		//autoplay
-		if (__this.o.autoPlay) { __this.autoPlay(); }
+		if (self.o.autoPlay) { self.autoPlay(); }
 		
 		
 		/*
 		 * Click Event
 		 */
-		__this.$paginationItem.on('click', function(e){
-			__this.moveBind(__this.$paginationItem.index(this));
+		self.$paginationItem.on('click', function(e){
+			self.moveBind(self.$paginationItem.index(this));
 			e.preventDefault();
 		});
 		
-		__this.$prevNavi.on('click', function(e){
-			__this.moveBind(__this.index - 1);
+		self.$prevNavi.on('click', function(e){
+			self.moveBind(self.index - 1);
 			e.preventDefault();
 		});
 		
-		__this.$nextNavi.on('click', function(e){
-			__this.moveBind(__this.index + 1);
+		self.$nextNavi.on('click', function(e){
+			self.moveBind(self.index + 1);
 			e.preventDefault();
 		});
 		
@@ -135,11 +135,11 @@ Carousel.prototype = {
 			}
 			
 			_timer = setTimeout(function () {
-				__this.elementWidth = __this.$element.outerWidth(true);
-				if (__this.o.loop) {
-					__this.makeClone();
+				self.elementWidth = self.$element.outerWidth(true);
+				if (self.o.loop) {
+					self.makeClone();
 				}
-				__this.setListStyle();
+				self.setListStyle();
 				
 				_timer = null;
 			}, _INTERVAL);
@@ -149,25 +149,25 @@ Carousel.prototype = {
 	
 	/**
 	 * indexUpdate
-	 * 引数で送られたindexを使って__this.indexを更新する。
+	 * 引数で送られたindexを使ってself.indexを更新する。
 	 * $itemの最大値より大きければ最小値にリセット、最小値より小さければ最大値にリセット
-	 * @param {number} index __this.indexの値をこの値に書き換える。0から始まる
+	 * @param {number} index self.indexの値をこの値に書き換える。0から始まる
 	 * @param {string} moved 'moved'を指定するとリセットの幅が狭まる。移動後の位置リセットに使用
 	 * @see init, move
 	 */
 	indexUpdate: function (index, moved) {
-		var __this = this;
+		var self = this;
 		
-		if (!__this.isMoving) {
+		if (!self.isMoving) {
 			//loopが有効 && moveする前なら
-			if (__this.o.loop && moved !== 'moved') {
-				if (index < -1                     ){ index = __this.$item.length - 1; }
-				if (index > __this.$item.length    ){ index = 0; }
+			if (self.o.loop && moved !== 'moved') {
+				if (index < -1                     ){ index = self.$item.length - 1; }
+				if (index > self.$item.length    ){ index = 0; }
 			} else {
-				if (    index < 0                      ){ index = __this.$item.length - 1; }
-				if (    index > __this.$item.length - 1){ index = 0; }
+				if (    index < 0                      ){ index = self.$item.length - 1; }
+				if (    index > self.$item.length - 1){ index = 0; }
 			}
-			__this.index = index;
+			self.index = index;
 		}
 	}
 	,
@@ -178,28 +178,28 @@ Carousel.prototype = {
 	 * @see, init
 	 */
 	makeClone: function () {
-		var __this = this, i, j;
+		var self = this, i, j;
 		
 		//作成要素数
-		__this.clonePrependNum = __this.cloneAppendNum = Math.ceil(__this.elementWidth / __this.itemWidth);
+		self.clonePrependNum = self.cloneAppendNum = Math.ceil(self.elementWidth / self.itemWidth);
 		
 		//既に作成された要素があれば削除
-		__this.$list.find($('.' + __this.o.cloneClass)).remove();
+		self.$list.find($('.' + self.o.cloneClass)).remove();
 		
 		//prepend
-		for (i = 0, j = __this.$item.length - 1; i < __this.clonePrependNum; i++) {
-			__this.$list.prepend(
-				__this.$item.clone().addClass(__this.o.cloneClass).removeClass(__this.o.currentClass)[j]
+		for (i = 0, j = self.$item.length - 1; i < self.clonePrependNum; i++) {
+			self.$list.prepend(
+				self.$item.clone().addClass(self.o.cloneClass).removeClass(self.o.currentClass)[j]
 			);
-			(j <= 0)? j = __this.$item.length - 1 : j--;
+			(j <= 0)? j = self.$item.length - 1 : j--;
 		}
 		
 		//append
-		for (i = 0, j = 0; i < __this.cloneAppendNum; i++) {
-			__this.$list.append(
-				__this.$item.clone().addClass(__this.o.cloneClass).removeClass(__this.o.currentClass)[j]
+		for (i = 0, j = 0; i < self.cloneAppendNum; i++) {
+			self.$list.append(
+				self.$item.clone().addClass(self.o.cloneClass).removeClass(self.o.currentClass)[j]
 			);
-			(j >= __this.$item.length - 1)? j = 0 : j++;
+			(j >= self.$item.length - 1)? j = 0 : j++;
 		}
 	}
 	,
@@ -210,10 +210,10 @@ Carousel.prototype = {
 	 * @see init, move
 	 */
 	setListStyle: function () {
-		var __this = this;
-		__this.$list.css({
-			width: __this.calcListWidth() + 'px',
-			marginLeft: __this.calcListMarginLeft() + 'px'
+		var self = this;
+		self.$list.css({
+			width: self.calcListWidth() + 'px',
+			marginLeft: self.calcListMarginLeft() + 'px'
 		});
 	}
 	,
@@ -225,8 +225,8 @@ Carousel.prototype = {
 	 * @see setListStyle
 	 */
 	calcListWidth: function () {
-		var __this = this;
-		return (__this.$item.length + __this.clonePrependNum + __this.cloneAppendNum ) * __this.itemWidth;
+		var self = this;
+		return (self.$item.length + self.clonePrependNum + self.cloneAppendNum ) * self.itemWidth;
 	}
 	,
 	
@@ -237,8 +237,8 @@ Carousel.prototype = {
 	 * @see setListStyle, move
 	 */
 	calcListMarginLeft: function () {
-		var __this = this;
-		return  - ( (__this.itemWidth * (__this.index + __this.clonePrependNum)) - __this.calcPos_x() );
+		var self = this;
+		return  - ( (self.itemWidth * (self.index + self.clonePrependNum)) - self.calcPos_x() );
 	}
 	,
 	
@@ -249,25 +249,25 @@ Carousel.prototype = {
 	 * @see calcListMarginLeft
 	 */
 	calcPos_x: function () {
-		var __this = this;
+		var self = this;
 		//numberであればnumberをそのまま返す
-		if (!isNaN(__this.o.pos_x)) {
-			return __this.o.pos_x + __this.o.pos_x_fix;
+		if (!isNaN(self.o.pos_x)) {
+			return self.o.pos_x + self.o.pos_x_fix;
 		//functionであれば実行した値を返す
-		} else if ($.isFunction(__this.o.pos_x)) {
-			return __this.o.pos_x() + __this.o.pos_x_fix;
+		} else if ($.isFunction(self.o.pos_x)) {
+			return self.o.pos_x() + self.o.pos_x_fix;
 		} else {
-			switch (__this.o.pos_x){
+			switch (self.o.pos_x){
 				case 'left':
-					return 0 + __this.o.pos_x_fix;
+					return 0 + self.o.pos_x_fix;
 					break;
 					
 				case 'center':
-					return (__this.elementWidth / 2) - (__this.itemWidth / 2) + __this.o.pos_x_fix;
+					return (self.elementWidth / 2) - (self.itemWidth / 2) + self.o.pos_x_fix;
 					break;
 					
 				case 'right':
-					return (__this.elementWidth - __this.itemWidth) + __this.o.pos_x_fix;
+					return (self.elementWidth - self.itemWidth) + self.o.pos_x_fix;
 					break;
 					
 				default:
@@ -281,21 +281,21 @@ Carousel.prototype = {
 	/**
 	 * moveBind
 	 * カルーセル移動, カレント表示を1つにバインド
-	 * @param {number} index __this.indexの値をこの値に書き換える。0から始まる
+	 * @param {number} index self.indexの値をこの値に書き換える。0から始まる
 	 * @see init, autoPlay
 	 */
 	moveBind: function (index) {
-		var __this = this;
+		var self = this;
 		
 		//index番号を更新
-		__this.indexUpdate(index);
+		self.indexUpdate(index);
 		
 		//移動前にcurrent表示
-		__this.addCurrentClass();
-		if (__this.o.currentHighlight) { __this.highlightEffect(); }
+		self.addCurrentClass();
+		if (self.o.currentHighlight) { self.highlightEffect(); }
 		
 		//移動
-		__this.move();
+		self.move();
 	}
 	,
 	
@@ -305,26 +305,26 @@ Carousel.prototype = {
 	 * @see moveBind
 	 */
 	move: function () {
-		var __this = this;
-		if (!__this.isMoving) {
-			__this.isMoving = true;
-			__this.$list.animate({
-				marginLeft: __this.calcListMarginLeft() + 'px'
+		var self = this;
+		if (!self.isMoving) {
+			self.isMoving = true;
+			self.$list.animate({
+				marginLeft: self.calcListMarginLeft() + 'px'
 			}, {
-				duration: __this.o.speed,
-				easing: __this.o.easing,
+				duration: self.o.speed,
+				easing: self.o.easing,
 				complete: function(){
-					__this.isMoving = false;
-					if (__this.o.loop) {
+					self.isMoving = false;
+					if (self.o.loop) {
 						//indexを更新
-						__this.indexUpdate(__this.index, 'moved');
+						self.indexUpdate(self.index, 'moved');
 						//位置をリセット
-						__this.setListStyle();
+						self.setListStyle();
 					}
 					
 					//移動後にcurrent表示
-					__this.addCurrentClass();
-					if (__this.o.currentHighlight) { __this.highlightEffect(); }
+					self.addCurrentClass();
+					if (self.o.currentHighlight) { self.highlightEffect(); }
 				},
 				queue: false
 			})
@@ -338,22 +338,22 @@ Carousel.prototype = {
 	 * @see init, move
 	 */
 	addCurrentClass: function () {
-		var __this = this;
+		var self = this;
 		
 		//currentClass削除
-		__this.$allList.children().removeClass(__this.o.currentClass);
+		self.$allList.children().removeClass(self.o.currentClass);
 		
 		//$itemをcurrent
-		__this.$list.children().eq(__this.clonePrependNum  + __this.index).addClass(__this.o.currentClass);
+		self.$list.children().eq(self.clonePrependNum  + self.index).addClass(self.o.currentClass);
 		
 		//$itemの最大値より大きい場合は、0番目の$paginationItemをcurrent
-		if (__this.index > __this.$item.length - 1) {
-			__this.$paginationItem.eq(0).addClass(__this.o.currentClass);
+		if (self.index > self.$item.length - 1) {
+			self.$paginationItem.eq(0).addClass(self.o.currentClass);
 		//$itemの最大値より小さい場合は、最後の$paginationItemをcurrent
-		} else if (__this.index < 0) {
-			__this.$paginationItem.eq(__this.$item.length - 1).addClass(__this.o.currentClass);
+		} else if (self.index < 0) {
+			self.$paginationItem.eq(self.$item.length - 1).addClass(self.o.currentClass);
 		} else {
-			__this.$paginationItem.eq(__this.index).addClass(__this.o.currentClass);
+			self.$paginationItem.eq(self.index).addClass(self.o.currentClass);
 		}
 	}
 	,
@@ -364,9 +364,9 @@ Carousel.prototype = {
 	 * @see init, move
 	 */
 	highlightEffect: function () {
-		var __this = this;
-		__this.$paginationItem.animate({opacity: 0.4}, {duration: 300, queue: false});
-		__this.$paginationItem + $('.' + __this.o.currentClass).animate({opacity: 1}, {duration: 300, queue: false});
+		var self = this;
+		self.$paginationItem.animate({opacity: 0.4}, {duration: 300, queue: false});
+		self.$paginationItem + $('.' + self.o.currentClass).animate({opacity: 1}, {duration: 300, queue: false});
 	}
 	,
 	
@@ -376,19 +376,19 @@ Carousel.prototype = {
 	 * @see init
 	 */
 	autoPlay: function () {
-		var __this = this, timer, autoPlay, autoPlayInterval;
-		autoPlayInterval = (__this.o.autoPlayInterval >= 40) ? __this.o.autoPlayInterval : 40;
+		var self = this, timer, autoPlay, autoPlayInterval;
+		autoPlayInterval = (self.o.autoPlayInterval >= 40) ? self.o.autoPlayInterval : 40;
 		
 		autoPlay = function(){
-			__this.moveBind(__this.index + 1);
+			self.moveBind(self.index + 1);
 		};
 		setTimeout(function () {
 			timer = setInterval(autoPlay, autoPlayInterval);
-		}, __this.o.autoPlayStartDelay);
+		}, self.o.autoPlayStartDelay);
 		
 		//マウスオーバーされている間はautoPlayを停止。
-		if (__this.o.autoPlayHoverStop) {
-			__this.$allListAndNavi.hover(
+		if (self.o.autoPlayHoverStop) {
+			self.$allListAndNavi.hover(
 				function(){
 					clearInterval(timer);
 				},
